8fc422fd68ad71a9d71f1278db91e5683ccea525
huzaifa <107106387+huzaifaah@users.noreply.github.com>
2022-06-12 14:08:10 +0500

    HuzaifaAfzal

---
+++ Operators.java
@@ -0,0 +1,19 @@
+public class Operators
+{
+ 	public static void main(String args[]) 
+	{
+		int x = 6;
+ 		int y = 4;
+ 		int z = 10;
+ 		x += 6;
+ 		y *= 5;
+ 		z += x * y;
+ 		System.out.println("x = " + x);
+ 		System.out.println("y = " + y);
+ 		System.out.println("z = " + z);
+ 	}
+}
+
+
+
+
